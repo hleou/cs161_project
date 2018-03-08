69bf583ea6cbaad88a559bbdb687e6d3b49a9881
hleou <hleou@stanford.edu>
2018-03-08 14:49:09 -0800

    CLCS - Slow Implementation
    Part (A) of Programming Part

---
+++ project/CLCSSlow.py
@@ -0,0 +1,27 @@
+import sys
+from LCS import LCS
+
+def cut(string, k):
+	return string[k:]+string[0:k]
+
+def CLCS(A, B):
+	m = len(A)
+	n = len(B)
+	max_len = -float('inf')
+	for k in range(m):
+		len_ = LCS(cut(A, k), B)
+		if len_ > max_len:
+			max_len = len_
+	return max_len
+
+def main():
+	if len(sys.argv) != 1:
+		sys.exit('Usage: `python CLCSSlow.py < input`')
+	
+	for l in sys.stdin:
+		A,B = l.split()
+		print CLCS(A,B)
+	return
+
+if __name__ == '__main__':
+	main()
