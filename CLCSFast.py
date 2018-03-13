e6190965d20307177185c20255645dff45416b5d
kalebmorris <kalebm@stanford.edu>
2018-03-13 00:16:06 -0700

    changed stuff

--- CLCSFast.py
+++ CLCSFast.py
@@ -6,17 +6,15 @@ from LCS import LCS_mod
 import time
 
 
-#def FindShortestPaths(A,B,AA,p,l,u,bestSoFar):
 def FindShortestPaths(A,B,AA,p,b,l,u,bestSoFar):
 	if u-l <= 1:
 		return
 	mid = (l+u)/2
-	#length = LCS_mod(A,B,AA,mid,p[l],p[u],p)
-	length = LCS_mod(A,B,AA,mid,p[l],p[u],p,b[l],b[u],b)
+
+	length = LCS_mod(A,B,AA,mid,p[u],p[l],p,b[l],b[u],b)
 	if length > bestSoFar[0]:
 		bestSoFar[0] = length
-	#FindShortestPaths(A,B,AA,p,l,mid,bestSoFar)
-	#FindShortestPaths(A,B,AA,p,mid,u,bestSoFar)
+
 	FindShortestPaths(A,B,AA,p,b,l,mid,bestSoFar)
 	FindShortestPaths(A,B,AA,p,b,mid,u,bestSoFar)
 
@@ -32,9 +30,9 @@ def FindShortestPaths1(A,B,AA,p):
 
 def CLCS(A,B):
 	m,n = len(A),len(B)
-	#path,bound,length = LCS_reg(A,B,True)
+
 	path,bound,length = LCS_reg(A,B,True)
-	#print bound
+
 	p = (m+1)*[np.zeros((2,n+1),dtype=int)]
 	b = (m+1)*[np.zeros((2,2*m+1),dtype=int)]
 
@@ -45,14 +43,15 @@ def CLCS(A,B):
 
 	bestSoFar = [length]
 	AA = A+A
-	#FindShortestPaths(A,B,AA,p,0,m,bestSoFar)
+
 	FindShortestPaths(A,B,AA,p,b,0,m,bestSoFar)
+	#print p
 	return bestSoFar[0]
 
 def main():
 	if len(sys.argv) != 1:
 		sys.exit('Usage: `python CLCSFast.py < input`')
-	#print CLCS('BANGLA','BEANQGR')
+	# print CLCS('ABC','CAB')
 	
 	for l in sys.stdin:
 	 	A,B = l.split()
--- LCS.py
+++ LCS.py
@@ -3,7 +3,6 @@ import numpy as np
 
 arr = np.zeros((2048, 2048), dtype=int)
 
-#def get_path_mod(AA,B,indx,p_lo,p_hi):
 def get_path_mod(AA,B,indx,p_lo,p_hi,b_lo,b_hi):
 	m = len(AA) / 2
 	n = len(B)
@@ -13,27 +12,22 @@ def get_path_mod(AA,B,indx,p_lo,p_hi,b_lo,b_hi):
 	path[0][j] = i
 	path[1][j] = i
 	bound = np.zeros((2,2*m+1))
-	bound[0][i] = j
-	bound[1][i] = j
 
 	while i>indx and j>0:
-		if AA[i-1] == B[j-1] and (i-1 <= p_hi[1][j-1] and i-1 >= p_lo[0][j-1]): # move diagonal
+		if AA[i-1] == B[j-1] and (i-1) >= p_hi[0][j-1] and (i-1) <= p_lo[1][j-1]: # move diagonal
 			i -= 1
 			j -= 1
 			path[0][j] = i
 			path[1][j] = i
-			bound[0][i] = j
-			bound[1][i] = j
-		elif arr[i-1][j] > arr[i][j-1] and (i-1 <= p_hi[1][j] and i-1 >= p_lo[0][j]): # move up, better option
+
+		elif arr[i-1][j] > arr[i][j-1] and (i-1) >= p_hi[0][j] and (i-1) <= p_lo[1][j]: # move up, better option
 			i -= 1
 			path[0][j] = i
-			bound[0][i] = j
-			bound[1][i] = j
+
 		else: # move left, better option
 			j -= 1
 			path[0][j] = i
 			path[1][j] = i
-			bound[0][i] = j
 			
 	if i > indx: # moved all the way left, so move up until you are at (index+1,1)
 		path[0][0] = indx
@@ -41,7 +35,6 @@ def get_path_mod(AA,B,indx,p_lo,p_hi,b_lo,b_hi):
 	if j > 0: # moved all the way up, so move left until you are at (index+1,1)
 		path[0][path[0]==0] = indx
 		path[1][path[1]==0] = indx
-		bound[1][indx] =  j
 
 	return path,bound
 
@@ -54,8 +47,6 @@ def get_path_reg(A,B):
 	path[0][j] = i
 	path[1][j] = i
 	bound = np.zeros((2,m+1))
-	bound[0][i] = j
-	bound[1][i] = j
 
 	while i>0 and j>0:
 		if A[i-1] == B[j-1]: # move diagonal
@@ -63,51 +54,44 @@ def get_path_reg(A,B):
 			j -= 1
 			path[0][j] = i
 			path[1][j] = i
-			bound[0][i] = j
-			bound[1][i] = j
+
 		elif arr[i-1][j] > arr[i][j-1]: # move up
 			i -= 1
 			path[0][j] = i
-			bound[0][i] = j
-			bound[1][i] = j
+
 		else: # move left
 			j -= 1
 			path[0][j] = i
 			path[1][j] = i
-			bound[0][i] = j
-
-	bounds1 = np.zeros((2,m+1))
-	bounds2 = np.zeros((2,m+1))
-
-	if j > 0: # moved all the way up, so move left until you are at (index+1,1)
-		bound[1][0] =  j
 
 	return path,bound
 
-#def LCS_mod(A,B,AA,indx,p_lo,p_hi,p):
 def LCS_mod(A,B,AA,indx,p_lo,p_hi,p,b_lo,b_hi,b):
 	m = len(A)
 	n = len(B)
 
-	#for i in range(indx,indx+m+1):
-		#for j in range(1:n+1):
-			#arr[i][j] = 0
-	for j in range(1,n+1):
-		arr[indx][j] = 0
+	for i in range(indx,indx+m+1):
+		for j in range(1,n+1):
+			arr[i][j] = 0
+
+	leftColCounter = 1
+	rightColCounter = n+1
 
 	for i in range(indx+1,indx+m+1): # refill the area from (mXn) region w/ left, top corner (m+indx,0)
-		for j in range(1,n+1):
+		for j in range(leftColCounter,n+1):
+
+			if p_lo[1][j] == i:
+				leftColCounter += 1
 
 			if AA[i-1] == B[j-1]:
 				arr[i][j] = arr[i-1][j-1]+1
 			else:
 				arr[i][j] = max(arr[i][j-1],arr[i-1][j])
-	if p:
-		#p[indx] = get_path_mod(AA,B,indx,p_lo,p_hi)
-		p[indx],b[indx] = get_path_mod(AA,B,indx,p_lo,p_hi,b_lo,b_hi)
-		return arr[m+indx][n] # return tuple (lcs, path, lcs length)
-	else: 
-		return arr[m+indx][n] # return lcs length
+
+	p[indx],b[indx] = get_path_mod(AA,B,indx,p_lo,p_hi,b_lo,b_hi)
+
+	return arr[m+indx][n] # return tuple (lcs, path, lcs length)
+
 
 
 def LCS_reg(A,B,path): #path is whether or not you want the retraced path
